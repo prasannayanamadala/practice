81a83d2eeb17fae513f95818202935d4450dd914
prasannaraavi <raaviprasanna9@gmail.com>
2022-04-03 13:23:10 -0400

    changes

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/prasannayanamadala/new/practice/interface"
+import 
+
+//"github.com/prasannayanamadala/new/practice/interface"
+"github.com/prasannayanamadala/new/practice/cgo"
 
 func main() {
 
-	interfaces.Test_Interfaces()
+	//interfaces.Test_Interfaces()
+	cgo_test.PracTest()
 }
